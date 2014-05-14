b29443e2291b54a96441aa1cfc5344573d1a4039
tcywinski <tcywinski@sigma.ug.edu.pl>
2014-05-14 23:17:30 +0200

    travis please accept it

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -6,16 +6,28 @@ describe MoviesController do
     @movie = Tmdb::Movie.detail(68721)
   end
 
-  it "should return a id" do
+  it "should return an id" do
     @movie.id.should eq(68721)
+  end
+
+  it "should id be int" do
+    @movie.id.is_a? Integer
   end
 
   it "should return a imdb id" do
     @movie.imdb_id.should eq("tt1300854")
   end
 
-  it "should return a original title" do
+  it "should imdb id be string" do
+    @movie.imdb_id.is_a? String
+  end
+
+  it "should return an original title" do
     @movie.original_title.should eq("Iron Man 3")
+  end
+
+  it "should original title be string" do
+    @movie.original_title.is_a? String
   end
 
   it "should return if for adults" do
@@ -26,6 +38,10 @@ describe MoviesController do
     @movie.backdrop_path.should eq("/n9X2DKItL3V0yq1q1jrk8z5UAki.jpg")
   end
 
+  it "should backdrop path be string" do
+    @movie.backdrop_path.is_a? String
+  end
+
   it "should return if belongs to collection" do
     @movie.belongs_to_collection.blank?
   end
@@ -34,16 +50,28 @@ describe MoviesController do
     @movie.budget.should eq(200000000)
   end
 
+  it "should budget be int" do
+    @movie.budget.is_a? Integer
+  end
+
   it "should return a homepage" do
     @movie.homepage.should eq("http://marvel.com/ironman3")
   end
 
-  # it "should return a popularity" do
-  #   @movie.popularity.should eq("Iron Man 3")
-  # end
+  it "should homepage be string" do
+    @movie.homepage.is_a? String
+  end
+
+  it "popularity should be int" do
+    @movie.popularity.is_a? Integer
+  end
 
   it "should return a poster path" do
     @movie.poster_path.should eq("/1Ilv6ryHUv6rt9zIsbSEJUmmbEi.jpg")
+  end
+
+  it "should poster path be string" do
+    @movie.poster_path.is_a? String
   end
 
   it "should return a release date" do
@@ -54,28 +82,56 @@ describe MoviesController do
     @movie.title.should eq("Iron Man 3")
   end
 
+  it "should title be string" do
+    @movie.title.is_a? String
+  end
+
   it "should return a revenue" do
     @movie.revenue.should eq(1198459803)
+  end
+
+  it "should revenue be int" do
+    @movie.revenue.is_a? Integer
   end
 
   it "should return a tagline" do
     @movie.tagline.should eq("Unleash the power behind the armor.")
   end
 
+  it "should tagline be string" do
+    @movie.tagline.is_a? String
+  end
+
   it "should return a runtime" do
     @movie.runtime.should eq(130)
+  end
+
+  it "should runtime be int" do
+    @movie.runtime.is_a? Integer
   end
 
   it "should return a vote_count" do
     @movie.vote_count.should eq(4462)
   end
 
+  it "should vote_count be int" do
+    @movie.vote_count.is_a? Integer
+  end
+
   it "should return a vote_average" do
     @movie.vote_average.should eq(6.8)
   end
 
+  it "should vote_average be int" do
+    @movie.vote_average.is_a? Integer
+  end
+
   it "should return a status" do
     @movie.status.should eq("Released")
+  end
+
+  it "should status be string" do
+    @movie.status.is_a? String
   end
     
 end
--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -6,8 +6,12 @@ describe PeopleController do
     @person = Tmdb::Person.detail(287)
   end
 
-  it "should return a id" do
+  it "should return an id" do
     @person.id.should eq(287)
+  end
+
+  it "should an id be int" do
+    @person.id.is_a? Integer
   end
 
   it "should return a false" do
@@ -22,8 +26,16 @@ describe PeopleController do
     @person.name.should eq("Brad Pitt")
   end
 
+  it "should a name be string" do
+    @person.name.is_a? String
+  end
+
   it "should return a place_of_birth" do
     @person.place_of_birth.should eq("Shawnee - Oklahoma - USA")
+  end
+
+  it "should a place_of_birth be string" do
+    @person.place_of_birth.is_a? String
   end
 
   it "should return a biography " do
@@ -34,16 +46,32 @@ describe PeopleController do
     @person.birthday.should eq("1963-12-18")
   end
 
+  it "should a birthday be string" do
+    @person.birthday.is_a? String
+  end
+
   it "should return a deathday" do
     @person.deathday.should eq("")
+  end
+
+  it "should a deathday be string" do
+    @person.deathday.is_a? String
   end
 
   it "should return a homepage" do
     @person.homepage.should eq("")
   end
 
+  it "should a homepage be string" do
+    @person.homepage.is_a? String
+  end
+
   it "should return a profile_path" do
     @person.profile_path.should eq("/kc3M04QQAuZ9woUvH3Ju5T7ZqG5.jpg")
+  end
+
+  it "should a profile_path be string" do
+    @person.profile_path.is_a? String
   end
 
 end
