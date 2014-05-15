e34fa34ec893b3d1e9ef0161caae81d20fb8914f
tcywinski <tcywinski@sigma.ug.edu.pl>
2014-05-15 13:54:21 +0200

    pierwszy test dla views

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -6,48 +6,6 @@ describe "home/index.html.erb" do
     @now_playing = Tmdb::Movie.now_playing
   end
 
-  # it "displays header" do
-  #   render
-  #   expect(rendered).to include("Editing gist")
-  # end
-
-  # it "displays lang headline" do
-  #   render
-  #   expect(rendered).to include("Lang")
-  # end
-
-  # it "displays description headline" do
-  #   render
-  #   expect(rendered).to include("Description")
-  # end
- 
-  # it "renders _form partial" do
-  #   render
-  #   expect(view).to render_template(:partial => "_form", :count => 1)
-  # end
- 
-  # it "displays Snippet data" do
-  #   render
-  #   expect(rendered).to include("snippet data")
-  # end
-
-  # it "displays description data" do
-  #   render
-  #   expect(rendered).to include("description data")
-  # end
- 
-  # it "displays description data" do
-  #   render
-  #   expect(rendered).to include("Description")
-  #   expect(rendered).to include("description data")
-  # end
-
-  # it "displays snippet data" do 
-  #   render
-  #   expect(rendered).to include("Snippet")
-  #   expect(rendered).to include("snippet data")
-  # end
-
   it "displays search" do
     render
     expect(rendered).to include("Playing")
